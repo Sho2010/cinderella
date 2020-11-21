1b707cced8e661ce4f9a001266294ce38cc0cd35
sho naito <sho20100@gmail.com>
2020-11-21 15:51:05 +0900

    Add kubebuilder:subresource:status to Cinderella resource

--- api/v1alpha1/cinderella_types.go
+++ api/v1alpha1/cinderella_types.go
@@ -20,12 +20,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // CinderellaSpec defines the desired state of Cinderella
 type CinderellaSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Foo is an example field of Cinderella. Edit Cinderella_types.go to remove/update
@@ -39,6 +37,7 @@ type CinderellaStatus struct {
 }
 
 // +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
 
 // Cinderella is the Schema for the cinderellas API
 type Cinderella struct {
